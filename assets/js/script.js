1ecae409d403ebc3eda4f5763b5a25070057846d
Topash15 <alextopash@gmail.com>
2021-07-10 19:12:29 -0400

    added form functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,11 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler =  function() {
+var createTaskHandler =  function(event) {
+
+    //ignores the browser default behavior to refresh page when submitted
+    event.preventDefault();
+
     //creates an object<li>
     var listItemEl = document.createElement("li");
     //adds class property
@@ -13,4 +17,4 @@ var createTaskHandler =  function() {
 }
 
 //listens for button to be clicked and exectutes function
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
